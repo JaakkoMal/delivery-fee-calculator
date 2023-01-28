1089a2f9bda90ff8abfe9ad125936e721c4271dc
JaakkoMal <j.j.malmi@gmail.com>
2023-01-28 11:09:04 +0200

    calculateDelieryCost adjusted
    Moved calculateDeliveryCost function to functions folder and gave it a return value to be more easily tested.

--- src/containers/CalculatorContainer.tsx
+++ src/containers/CalculatorContainer.tsx
@@ -1,15 +1,7 @@
 import { useState } from 'react'
 import { Calculator } from '../components/Calculator'
 import { OrderInfo } from '../types/Types'
-import { 
-    isCartValueHundredOrMore,
-    addSmallOrderSurchargeIfNeeded,
-    addFeeForEveryBeginning500m,
-    isDeliveryCostAtMaximum,
-    addFeeForMultipleItems,
-    isFridayRush,
-    addRushTimeFee
-} from '../utils/functions'
+import { calculateDeliveryCost } from '../utils/functions'
 
 const initialOrderInfo: OrderInfo = {
     cartValue: 0,
@@ -32,7 +24,7 @@ export function CalculatorContainer() {
     const onChangeDeliveryDistance = (e: React.ChangeEvent<HTMLInputElement> ) => {
         e.preventDefault()
         const newDeliveryDistance = Number(e.target.value)
-        if(!Number.isNaN(newDeliveryDistance)){
+        if (!Number.isNaN(newDeliveryDistance)) {
             console.log(newDeliveryDistance)
             setOrderInfo(prev => { return {...prev, deliveryDistance: newDeliveryDistance}})
         } else {
@@ -43,7 +35,7 @@ export function CalculatorContainer() {
     const onChangeAmountItems = (e: React.ChangeEvent<HTMLInputElement> ) => {
         e.preventDefault()
         const newAmountItems = Number(e.target.value)
-        if(!Number.isNaN(newAmountItems)){
+        if (!Number.isNaN(newAmountItems)) {
             setOrderInfo(prev => {return {...prev, amountItems: newAmountItems}})
         } else {
             console.log("Not a number (amount items).")
@@ -58,33 +50,8 @@ export function CalculatorContainer() {
         setOrderInfo(prev => { return {...prev, orderTime: newOrderTime}})
     }
 
-    const calculateDeliveryCost = (fullOrderInfo: OrderInfo) => {
-        let totalDeliveryCost: number = 2
-        const isFreeDelivery = isCartValueHundredOrMore(fullOrderInfo.cartValue)
-
-        if (isFreeDelivery) {
-            setOrderInfo(prev => { return {...prev, deliveryCost: 0}})
-            return
-        }
-        
-        totalDeliveryCost += addSmallOrderSurchargeIfNeeded(fullOrderInfo.cartValue)
-        totalDeliveryCost += addFeeForEveryBeginning500m(fullOrderInfo.deliveryDistance)
-
-        if (!isDeliveryCostAtMaximum(totalDeliveryCost)) {
-            totalDeliveryCost += addFeeForMultipleItems(fullOrderInfo.amountItems)
-        }
-
-        if (!isDeliveryCostAtMaximum(totalDeliveryCost)) {
-            if (isFridayRush(fullOrderInfo.orderDate, fullOrderInfo.orderTime)) {
-                totalDeliveryCost = addRushTimeFee(totalDeliveryCost)
-            }
-        }
-
-        if (!isDeliveryCostAtMaximum(totalDeliveryCost)) {
-            setOrderInfo(prev => { return {...prev, deliveryCost: totalDeliveryCost}})
-        } else {
-            setOrderInfo(prev => { return {...prev, deliveryCost: 15}})
-        }
+    const onChangeDeliveryCost = (fullOrderInfo: OrderInfo) => {
+        setOrderInfo(prev => { return {...prev, deliveryCost: calculateDeliveryCost(fullOrderInfo)}})
     }
 
     return (
@@ -95,7 +62,7 @@ export function CalculatorContainer() {
         onChangeAmountItems={onChangeAmountItems}
         onChangeOrderDate={onChangeOrderDate}
         onChangeOrderTime={onChangeOrderTime}
-        calculateDeliveryCost={calculateDeliveryCost}
+        onChangeDeliveryCost={onChangeDeliveryCost}
         />
     )
 }
